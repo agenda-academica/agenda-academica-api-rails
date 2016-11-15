c86eeb26bba650f893b3cccfc368ee4e3cc79bac
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-11-15 19:40:31 -0200

    Fix send_test_notification method.

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,24 +1,26 @@
 require 'exponent-server-sdk'
 
 class TokensController < ApplicationController
-  after_action :verify_authorized
-  after_action :verify_authorized, except: %i[send_test_notification]
+  after_action :verify_authorized, except: %i[create send_test_notification]
   after_action :verify_policy_scoped, only: %i[]
 
   def create
-    @token = Token.new(token_params)
-    @token.user = current_user
-    @token.save!
+    unless Token.where(user: current_user).first
+      @token = Token.new(token_params)
+      @token.user = current_user
+      @token.save!
+    end
 
     render json: { success: true }
   end
 
   def send_test_notification
-    @token = Token.where(user: current_user).first
+    @user = User.where(email: params[:email]).first
+    @token = Token.where(user: @user).first
 
     exponent.publish(
       exponentPushToken: @token.value,
-      message: 'Send Push Notification Test',
+      message: params[:message],
       data: { foo: 'bar' },
     )
 
