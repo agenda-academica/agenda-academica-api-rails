b01509cd171ede7c9ce8d904077d0d34b879ce09
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-11-17 01:07:28 -0200

    Add evento migration.

--- db/migrate/20161006221652_create_eventos.rb
+++ db/migrate/20161006221652_create_eventos.rb
@@ -1,14 +1,16 @@
 class CreateEventos < ActiveRecord::Migration
   def change
     create_table :eventos do |t|
-      t.references :disciplina, index: true, foreign_key: true
+      t.references :disciplina, index: true, foreign_key: true, null: true
       t.string :tipo
+      t.string :cor
       t.string :titulo
       t.string :descricao
-      t.date :dataInicio
-      t.date :dataFim
-      t.time :horaInicio
-      t.time :horaFim
+      t.date :data_inicio
+      t.date :data_fim
+      t.time :hora_inicio
+      t.time :hora_fim
+      t.time :notify_at, null: false, default: '00:15:00'
 
       t.timestamps null: false
     end
