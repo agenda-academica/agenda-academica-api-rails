e5da4556947909a38b747058bc03f90c2ea2dd1c
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-11-20 19:22:47 -0200

    Add redis-namespace to handle better redis keysi by environment.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,9 @@
+uri = URI.parse(ENV["REDIS_URL"])
+redis_connection = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
 if Rails.env.production?
-  uri = URI.parse(ENV["REDIS_URL"])
-  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  Resque.redis = redis_connection
+else
+  require 'redis-namespace'
+  Resque.redis = Redis::Namespace.new(ENV['REDIS_NAMESPACE'], :redis => redis_connection)
 end
